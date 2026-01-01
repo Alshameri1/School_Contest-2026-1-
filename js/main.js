098c0ef1c2223ec80dc4c92be372d8c3f89e0c37
Ahmed <robensz2022@gmail.com>
2026-01-01 13:12:49 +0300

    Some Changes

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@
 Object.keys(window.localStorage).forEach(key => {
     document.documentElement.style.setProperty(key, window.localStorage.getItem(key))
 })
+
+
+// Header Scroll 
+let header = document.querySelector('#header')
+let lastScroll = window.pageYOffset;
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+
+    if (currentScroll > lastScroll) {
+        header.style.cssText = 'opacity: 0; top: -30%; '
+    } else if (currentScroll < lastScroll) {
+        header.style.cssText = 'top: 30px; opacity: 1;'
+    }
+    lastScroll = currentScroll;
+});
